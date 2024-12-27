0fcd4bf09023ccbc01f4914a3f6f6ba68129624a
Madiba Hudson-Quansah <mhquansah@gmail.com>
2024-12-27 03:34:30 +0000

    Wrapping my head around websockets

--- backend/src/routes/ws.ts
+++ backend/src/routes/ws.ts
@@ -3,29 +3,37 @@ import { createNodeWebSocket } from "@hono/node-ws";
 import type { SocketMessage } from "../types.js";
 import { MessageTypes } from "../constants/socketMessage.js";
 import { SocketService } from "../services/SocketService.js";
+import { customLogger } from "../logger.js";
+import { prettyJSON } from "hono/pretty-json";
+import { prettyPrint } from "../utils.js";
 
 const ws = new Hono();
 
 const { injectWebSocket, upgradeWebSocket } = createNodeWebSocket({ app: ws });
 
-const roomToUsersMap = new Map<string, WebSocket[]>();
 //map from the roomId to all the room sockets for that room
+const roomToUsersMap = new Map<string, WebSocket[]>();
 
-const roomToHostmap = new Map<string, WebSocket>();
 //map from the roomId to the host socket
-
+const roomToHostmap = new Map<string, WebSocket>();
 
 ws.get(
     "/ws",
     upgradeWebSocket((c) => ({
+        onOpen(event, socket) {
+            customLogger(`Connected`);
+        },
         onMessage(event, socket) {
             console.log(`Message from client: ${event.data}`);
-            
-            const message = JSON.parse(event.data.toString());
-            const { type, roomId, userId } = message;
-            
-            if(type == MessageTypes.JOIN) SocketService.handleJoinRoomMessage(roomId!, userId!, roomToHostmap);
-        
+
+            try {
+                const message = JSON.parse(event.data.toString());
+                const { type, roomId, userId } = message;
+
+                if (type == MessageTypes.JOIN) SocketService.handleJoinRoomMessage(roomId!, userId!, roomToHostmap);
+            } catch (e) {
+                customLogger(`Error on message -> ${e}`);
+            }
         },
         onClose: () => {
             console.log("Connection closed");
