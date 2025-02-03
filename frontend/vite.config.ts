aea15f6bf74323049acd790acc1f0f9002ebbfd9
Madiba Hudson-Quansah <mhquansah@gmail.com>
2025-02-03 20:27:02 +0000

    Tailwindcss setup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { viteStaticCopy } from "vite-plugin-static-copy";
+import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 // https://vite.dev/config/
@@ -17,6 +18,7 @@ export default defineConfig({
     },
     plugins: [
         react(),
+        tailwindcss(),
         viteStaticCopy({
             targets: [{ src: path.resolve(__dirname, "./CNAME"), dest: path.resolve(__dirname, "./dist") }],
         }),
